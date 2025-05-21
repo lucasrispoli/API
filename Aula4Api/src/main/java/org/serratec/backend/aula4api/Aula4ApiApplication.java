a2c56f214ba7d4ba03e05d651509d6f44e4204c3
Lucas Rispoli Delgado <162611605+lucasrispoli@users.noreply.github.com>
2025-05-21 20:35:42 -0300

    Add files via upload

---
+++ Aula4Api/src/main/java/org/serratec/backend/aula4api/Aula4ApiApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.aula4api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula4ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula4ApiApplication.class, args);
+    }
+
+}
